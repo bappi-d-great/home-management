e93d50c762e5a6439b9d5ce769ccdf6cd5899515
Ashok Nath <anath.awesomemotive.com>
2022-12-28 18:14:23 -0700

    Npm and prettier install

--- public/assets/js/App.js
+++ public/assets/js/App.js
@@ -1,49 +1,32 @@
 const Pet = (props) => {
-	return React.createElement(
-		'div',
-		{},
-		[
-			React.createElement('h1', {}, props.name),
-			React.createElement('h2', {}, props.animal),
-			React.createElement('h3', {}, props.location)
-		]
-	)
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.location),
+  ]);
+};
 
 const App = () => {
-	return React.createElement(
-		'div',
-		{},
-		[
-			React.createElement('h1', {}, 'Personal Management System'),
-			React.createElement(
-				Pet,
-				{
-					name: 'Tomi',
-					animal: 'Dog',
-					location: 'Alberta'
-				},
-			),
-			React.createElement(
-				Pet,
-				{
-					name: 'Swiss',
-					animal: 'Rabit',
-					location: 'Regina'
-				},
-			),
-			React.createElement(
-				Pet,
-				{
-					name: 'Chim',
-					animal: 'Cat',
-					location: 'Beijing'
-				},
-			)
-		]
-	);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Personal Management System"),
+    React.createElement(Pet, {
+      name: "Tomi",
+      animal: "Dog",
+      location: "Alberta",
+    }),
+    React.createElement(Pet, {
+      name: "Swiss",
+      animal: "Rabit",
+      location: "Regina",
+    }),
+    React.createElement(Pet, {
+      name: "Chim",
+      animal: "Cat",
+      location: "Beijing",
+    }),
+  ]);
+};
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
